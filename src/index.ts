aca2b7a45ab523cae98d7d69f8d5bbb43910c53c
Austin Hollopeter <34923886+Ajax-7@users.noreply.github.com>
2019-07-15 15:52:20 -0400

    first day changes

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,12 @@ console.log('Ready to Party');
 console.log(add(10, 18));
 
 console.log(PI);
+
+const nameInput = document.getElementById('name') as HTMLInputElement;
+const doitButton = document.getElementById('doit') as HTMLButtonElement;
+const messageoutput = document.getElementById('message');
+
+doitButton.addEventListener('click', function () {
+    const enteredname = nameInput.value;
+    messageoutput.innerText = enteredname.toUpperCase();
+})
